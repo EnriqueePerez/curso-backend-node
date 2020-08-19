4bef04bc5ffb89ec7e794243ee588c8dd7005645
EnriqueePerez <enrike_084@hotmail.com>
2020-08-19 14:59:09 -0500

    First route added, also added mockup data to work

--- movies-api/index.js
+++ movies-api/index.js
@@ -2,14 +2,9 @@ const express = require('express');
 const app = express();
 
 const { config } = require('./config/index'); //getting the config for the development enviroment
+const moviesApi = require('./routes/movies');
 
-app.get('/', function (req, res) {
-  res.send('Hello world');
-});
-
-app.get('/json', function (req, res) {
-  res.json({ Hello: 'world' });
-});
+moviesApi(app); //Using the movies routes in the app server
 
 app.listen(config.port, function () {
   console.log(`Listening on http://localhost:${config.port}`);
